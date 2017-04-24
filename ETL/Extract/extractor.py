c2e29945d0926c56af4236ae6e9e16b3b591fdc1
ndelatorre <ndelatorre@BRA009065-dev.jato.com.br>
2017-04-24 13:27:08 -0400

    Improved SqlDataExtractor._write_to_file() and corrected rt.incentives query

--- ETL/Extract/extractor.py
+++ ETL/Extract/extractor.py
@@ -6,7 +6,7 @@ import pymssql
 import os
 import access as acc
 
-raw_dir_path = '{}/weaver/etl/raw/'.format(os.path.expanduser('~'))
+extracted_dir_path = '{}/weaver/etl/extracted/'.format(os.path.expanduser('~'))
 
 
 class Extractor(metaclass=abc.ABCMeta):
@@ -36,17 +36,14 @@ class FtpExtractor(Extractor):
         Extracts (downloads) file from ftp
         :return: the path to the downloaded file
         """
-        try:
-            with ftplib.FTP(self.access.address) as ftp:
-                ftp.login(self.access.username, self.access.pwd)
-                local_file_path = '{0}{1}'.format(self.output_path, self.url.split('/')[-1])
-                if os.path.exists(local_file_path):
-                    os.remove(local_file_path)
-                with open(local_file_path, 'wb') as downloading_file:
-                    ftp.retrbinary('RETR {}'.format(self._get_file_path_since_root()), downloading_file.write)
-                return local_file_path
-        except Exception as ex:
-            raise ex
+        with ftplib.FTP(self.access.address) as ftp:
+            ftp.login(self.access.username, self.access.pwd)
+            local_file_path = '{0}{1}'.format(self.output_path, self.url.split('/')[-1])
+            if os.path.exists(local_file_path):
+                os.remove(local_file_path)
+            with open(local_file_path, 'wb') as downloading_file:
+                ftp.retrbinary('RETR {}'.format(self._get_file_path_since_root()), downloading_file.write)
+            return local_file_path
 
     def _get_file_path_since_root(self):
         url = self.url
@@ -55,32 +52,35 @@ class FtpExtractor(Extractor):
         return url.replace(url.split('/')[0], '')
 
 
-# TODO test
 class SqlDataExtractor(Extractor):
     def __init__(self, access: acc.Access, output_path: str, db: str, query: str):
         if os.path.exists(output_path) and os.path.isfile(output_path):
             os.remove(output_path)
+        else:
+            os.makedirs(output_path.replace(os.path.basename(output_path), ''), exist_ok=True)
         super().__init__(access, output_path)
         self.db = db
         self.query = query
 
     def extract(self, to_file: bool):
-        try:
-            with pymssql.connect(self.access.address, self.access.username, self.access.pwd, self.db,
-                                 charset='utf-8') as conn:
-                with conn.cursor() as cursor:
-                    cursor.execute(self.query)
-                    if to_file:
-                        self._write_to_file(cursor.fetchall())
-                    else:
-                        return cursor.fetchall()
-        except Exception as ex:
-            raise ex
+        """
+        Query database extracting results
+        :param to_file: output to file?
+        :return: the extracted results XOR the output path
+        """
+        # specifying charset param in pymssql.connect() raises an unknown error
+        with pymssql.connect(self.access.address, self.access.username, self.access.pwd, self.db) as conn:
+            with conn.cursor() as cursor:
+                cursor.execute(self.query)
+                if to_file:
+                    self._write_to_file(cursor.fetchall())
+                else:
+                    return cursor.fetchall()
 
-    def _write_to_file(self, data: tuple):
-        with open('{}{}/{}'.format(raw_dir_path, self.db, self.output_path), 'w') as result_file:
-            for row in data:
-                result_file.write('{}\n'.format('|'.join(row)))
+    def _write_to_file(self, query_results: tuple):
+        with open(self.output_path, 'w') as result_file:
+            for row in query_results:
+                result_file.write('{}{}'.format('|'.join(str(col) for col in row), '\n'))
 
 
 class MultiSourceExtractor(Extractor):
@@ -89,6 +89,11 @@ class MultiSourceExtractor(Extractor):
         self.extractors_list = single_extractor_list
 
     def extract(self, to_file: bool):
+        """
+        Loop through each source, extracting 
+        :param to_file: output to file?
+        :return: the extracted results
+        """
         if len(self.extractors_list) < 1:
             raise IndexError('MultiSourceExtractor\'s extractor list is empty')
         results = []
@@ -104,38 +109,38 @@ def extract(target: str):
     :param target: the immediate destination of this data
     :return: 
     """
-    def create_extractor(target: str):
+    def create_extractor():
         if target == 'sscbr_cs2002':
             return FtpExtractor(acc.access_dict['jatoftp2'],
                                 'ftp://ftp2.carspecs.jato.com/CURRENT/DATABASES/SQLSERVER/SSCBR/SSCBR_CS2002_SQL.EXE',
-                                '{}sscbr_cs2002/'.format(raw_dir_path))
+                                '{}sscbr_cs2002/'.format(extracted_dir_path))
         elif target == 'nscbr_cs2002':
             return FtpExtractor(acc.access_dict['jatoftp2'],
                                 'ftp://ftp2.carspecs.jato.com/CURRENT/DATABASES/SQLSERVER/NSCBR/NSCBR_CS2002_SQL.EXE',
-                                '{}nscbr_cs2002/'.format(raw_dir_path))
+                                '{}nscbr_cs2002/'.format(extracted_dir_path))
         elif target == 'escbr_cs2002':
             return FtpExtractor(acc.access_dict['jatoftp2'],
                                 'ftp://ftp2.carspecs.jato.com/CUSTOMEREMBARGO/Current/Databases/SQLSERVER/SSCBR/'
                                 'Incentive_Public_BR/SSCBR_CS2002_SQL.EXE',
-                                '{}escbr_cs2002/'.format(raw_dir_path))
+                                '{}escbr_cs2002/'.format(extracted_dir_path))
         elif target == 'rt.vehicles':
             extractors = (SqlDataExtractor(acc.access_dict['ukvsqlbdrep01'],
                                            db='rt',
-                                           query='select * vw_rt_vehicles_from_sscbr_cs2002'
-                                                 ' order by e.vehicle_id, e.schema_id',
-                                           output_path='sscbr_cs2002.txt'),
+                                           query='select * from vw_rt_vehicles_from_sscbr_cs2002'
+                                                 ' order by vehicle_id, schema_id',
+                                           output_path='{}rt/sscbr_cs2002.txt'.format(extracted_dir_path)),
                           SqlDataExtractor(acc.access_dict['ukvsqlbdrep01'],
                                            db='rt',
-                                           query='select * vw_rt_vehicles_from_sscbr_cs2002 '
-                                                 'order by e.vehicle_id, e.schema_id',
-                                           output_path='nscbr_cs2002.txt'))
+                                           query='select * from vw_rt_vehicles_from_nscbr_cs2002'
+                                                 ' order by vehicle_id, schema_id',
+                                           output_path='{}rt/nscbr_cs2002.txt'.format(extracted_dir_path)))
             return MultiSourceExtractor(single_extractor_list=extractors)
         elif target == 'rt.incentives':
             return SqlDataExtractor(acc.access_dict['ukvsqlbdrep01'],
                                     db='rt',
-                                    query='select * vw_rt_vehicles_from_escbr_cs2002_br_public_incentive'
-                                          ' order by e.vehicle_id, e.schema_id',
-                                    output_path='escbr_cs2002.txt')
+                                    query='select * from vw_rt_incentives_from_escbr_cs2002_br_public_incentive'
+                                          ' order by vehicle_id, schema_id, option_id',
+                                    output_path='{}rt/escbr_cs2002_br_public_incentive.txt'.format(extracted_dir_path))
 
-    extractor = create_extractor(target)
+    extractor = create_extractor()
     return extractor.extract(to_file=True)
