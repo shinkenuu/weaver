df78b3ce33dce45ef0acf17a009bfc52ca90af32
ndelatorre <ndelatorre@BRA009065-dev.jato.com.br>
2017-06-20 18:06:30 -0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+
+setup(name='weaver',
+      version='0.1dev',
+      packages=['etl', 'reporttool'],
+      licence='GPL',
+      long_description='Lorem ipsum dolor sit amet')
